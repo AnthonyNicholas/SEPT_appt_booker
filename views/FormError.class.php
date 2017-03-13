097d2ce984304f8d5ff256b0ff7dafc23e17b74b
rmit-s3448342-Jake-Williams <s3448342@student.rmit.edu.au>
2017-03-13 18:22:29 +1100

    Added FormError view class
    Will integrate with codebase after demo tomorrow

---
+++ views/FormError.class.php
@@ -0,0 +1,21 @@
+<?php
+
+class FormError
+{
+    private $error = array(
+                           "email" => "Did not enter a valid email",
+                           "fname" => "Did not enter a valid first name",
+                           "lname" => "Did not enter a valid last name",
+                           "password" => "Passwords do not match",
+                           "duplicate" => "An account with that email address already exists");
+
+    public function printHtml($errors)
+    {
+         echo "<div class = \"error_list\">";
+
+         for ($i = 0; $i < count($errors); $i++) 
+             echo "<div class = \"error_msg\"><li>".$this->error[$errors[$i]]."</li></div>"; 
+
+         echo "</div>";
+    }
+}
