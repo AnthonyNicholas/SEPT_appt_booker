d3d8e740065d25bfaf3346e328bb73fad691ae8e
Dan Ho <danny.ho9765@gmail.com>
2017-03-27 23:37:04 +0000

    tests so far

---
+++ tests/CustomerTest.php
@@ -0,0 +1,59 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once('models/Customer.class.php');
+
+class CustomerTest extends TestCase
+{
+
+    protected $email;
+    protected $fname;
+    protected $lname;
+    protected $address;
+    protected $phone;
+    protected $pword;
+
+    protected function setUp()
+    {
+        $this->email = 'amy@aardvark.com';
+        $this->fname = 'Amy';
+        $this->lname = 'Aardvark';
+        $this->address = '21 Aardvark Court, Melbourne, Vic 3000';
+        $this->phone = '99999999';
+        $this->pword = 'aardvark';
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+
+    public function testCreateCustomer()    {
+        //$stub = $this->createMock(Customer::class);
+        
+        // Insert test customer into database (constructor for customer retreives customer from DB).
+        
+        $ctrl = new Controller();
+        
+        $q = $ctrl->get_db()->prepare("INSERT INTO Customers (email, fName, lName, address, phoneNo, password) VALUES  (?, ?, ?, ?, ?, ?);");
+        $q->bind_param('ssssss', $this->email, $this->fname, $this->lname, $this->address, $this->phone, $this->pword);
+        $q->execute();
+
+        $testCustomer = new Customer($this->email,$ctrl->get_db());
+        
+        $this->assertEquals($testCustomer->data->fName, $this->fname);
+        
+        // Delete test record from DB        
+        $q = $ctrl->get_db()->prepare("DELETE FROM Customers WHERE email = ?;");
+        $q->bind_param('s', $this->email);
+        $q->execute();
+        
+    }
+    
+    public function testNotACustomer()    {
+        $email = 'xxx@xxx.com'
+        $ctrl = new Controller();
+        $testCustomer = new Customer($email,$ctrl->get_db());
+        $this->assertNull($testCustomer->data->fName);
+    }
+}
