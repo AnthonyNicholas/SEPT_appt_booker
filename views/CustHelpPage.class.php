a57317242b61cfbdcbe472db8e3d3b60160a6868
Dan Ho <danny.ho9765@gmail.com>
2017-04-05 04:48:21 +0000

    help pages

---
+++ views/CustHelpPage.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class CustHelpPage    {
+    public function printHtml()    {
+        ?>
+        <div>
+            <h1>How to make a booking</h1>
+        </div>
+        <div>
+            <p>Scroll to the staff member you would like to see</p>
+            <p>Find the day of the week and the time you would like to
+            see a staff member</p>
+            <p>Select by clicking on the time</p>
+        </div>
+        <?php
+    }
+}
