17d5762f7d0fc23bd32f972538acf9b4f17ea8d7
Dan Ho <danny.ho9765@gmail.com>
2017-04-08 10:23:15 +0000

    help imgs

--- views/CustHelpPage.class.php
+++ views/CustHelpPage.class.php
@@ -12,15 +12,20 @@ class CustHelpPage    {
         </div>
         <div>
             <p>Scroll to the staff member you would like to see</p>
+            <img src="/help-imgs/how2makebook.PNG" style="width:900px;height:305px;">
             <p>Find the day of the week and the time you would like to
             see a staff member</p>
-            <p>Select by clicking on the time</p>
+            <img src="/help-imgs/how2makebook1.PNG" style="width:900px;height:200px;">
+            <p>Select by clicking on the time, and confirm the booking. Taken
+            bookings will appear greyed out and cannot be selected</p>
+            <img src="/help-imgs/how2makebook2.PNG" style="width:900px;height:200px;">
+            <p>A success message will appear with your booking details</p>
         </div>
         <div>
             <h2>Viewing your current bookings</h2>
         </div>
         <div>
-            <p>Click on the 'My Bookings' tab</p>
+            <img src="/help-imgs/mybookings.PNG" style="width:900px;height:150px;">
             <p>Scroll through your current bookings. Details will include
             the time of the appointment with your staff member.</p>
         </div>
