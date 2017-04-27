b5de46dd8324025e45204cc3ab809ee4f6ed48bb
Adam Young <adamyoung012@gmail.com>
2017-04-28 05:08:07 +1000

    Added queries for checking whether an appointment can be booked to Calendar model and added a SLOT_LEN constant to config

--- config.php
+++ config.php
@@ -16,6 +16,7 @@ $config = array(
 
 define("CSS","css/");
 define("JS","js/");
+define("SLOT_LEN", 30);
 
 //set default timezone to maintain proper event calculations
 date_default_timezone_set("Australia/Melbourne");
