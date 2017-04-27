61d57e58cf776fcef84cdcc9cf3ee22ad06efcad
rmit-s3448342-Jake-Williams <s3448342@student.rmit.edu.au>
2017-04-27 01:14:05 +0000

    fixed nav issue

--- views/SiteContainer.class.php
+++ views/SiteContainer.class.php
@@ -113,10 +113,10 @@ public function printNav($type = "none")
             <?php if ($type == "owner"){ ?>
                 <ul class="nav navbar-nav">
                     <li><a href="mainPageOwner.php">View Bookings</a></li>
-                    <li><a href="../empOwnerAdd.php">Add Employee</a></li>
-                    <li><a href="../WorkerAvailability.php">Employee Times</a></li>
-                    <li><a href="../ownerCombinedCal.php">Combined calendar view</a></li>
-                    <li><a href="../helpPage.php">Help</a></li>
+                    <li><a href="empOwnerAdd.php">Add Employee</a></li>
+                    <li><a href="WorkerAvailability.php">Employee Times</a></li>
+                    <li><a href="ownerCombinedCal.php">Combined calendar view</a></li>
+                    <li><a href="helpPage.php">Help</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="logout.php">Log Out</a></li>
@@ -127,7 +127,7 @@ public function printNav($type = "none")
                 <ul class="nav navbar-nav">
                     <li><a href="mainPageCust.php">Book Appointment</a></li>
                     <li><a href="BookingSummary.php">My Bookings</a></li>
-                    <li><a href="../helpPage.php">Help</a></li>
+                    <li><a href="helpPage.php">Help</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="logout.php">Log Out</a></li>
