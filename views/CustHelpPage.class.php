091bac20b74746a859089b26c3013b250736139d
Dan Ho <danny.ho9765@gmail.com>
2017-04-29 11:14:26 +0000

    Updated help pages

--- views/CustHelpPage.class.php
+++ views/CustHelpPage.class.php
@@ -11,15 +11,15 @@ class CustHelpPage    {
             <h2>How to make a booking</h2>
         </div>
         <div>
-            <p>Scroll to the staff member you would like to see</p>
-            <img src="help-imgs/how2makebook.PNG" style="width:900px;height:305px;">
-            <p>Find the day of the week and the time you would like to
-            see a staff member</p>
-            <img src="help-imgs/how2makebook1.PNG" style="width:900px;height:200px;">
+            <img src="help-imgs/how3makebook.PNG" style="width:900px;height:210px;">
+            <p>Select an appointment type and the staff member you would like to see</p>
+            <img src="help-imgs/how3makebook1.PNG" style="width:900px;height:300px;">
             <p>Select by clicking on the time, and confirm the booking. Taken
             bookings will appear greyed out and cannot be selected</p>
-            <img src="help-imgs/how2makebook2.PNG" style="width:900px;height:200px;">
-            <p>A success message will appear with your booking details</p>
+            <img src="help-imgs/how3makebook2.PNG" style="width:900px;height:200px;">
+            <p>Confirm your appointment and type by clicking 'confirm'</p>
+            <img src="help-imgs/how3makebook3.PNG" style="width:900px;height:200px;">
+            <p>A confirmation message will appear</p>
         </div>
         <div>
             <h2>Viewing your current bookings</h2>
@@ -27,7 +27,8 @@ class CustHelpPage    {
         <div>
             <img src="help-imgs/mybookings.PNG" style="width:900px;height:150px;">
             <p>Scroll through your current bookings. Details will include
-            the time of the appointment with your staff member.</p>
+            the time of the appointment with your staff member and the appointment
+            type.</p>
         </div>
         </div>
         <?php
--- views/OwnerHelpPage.class.php
+++ views/OwnerHelpPage.class.php
@@ -19,36 +19,36 @@ class OwnerHelpPage    {
             <p>When a booking is taken the time a booking has been made is
             clickable and will take you to the details of booking, including
             the customer email and the customer name.</p>
-            <img src="help-imgs/empinfo.PNG"  style="width:800px;height:200px;">
+            <img src="help-imgs/empinfo.PNG"  style="width:900px;height:200px;">
         </div>
         
         <div>
             <h2>Adding Employees</h2>
         </div>
         <div>
-            <h3>You must add employees before you can set and view
-            their times.</h3>
+            <p>You must add employees before you can set and view
+            their times. Employees may have specialisations that customers
+            can select when they select an employee and appointment type.</p>
         </div>
         <div>
             <img src="help-imgs/add-emp.PNG" style="width:700px;height:90px;">
             <p>Click on the 'add employee' tab</p>
-            <img src="help-imgs/add-emp 2.PNG" style="width:700px;height:150px;">
-            <p>Enter the first and last name of the employee. Note that
-            both fields are required</p>
-            <img src="help-imgs/add-emp 3.PNG" style="width:700px;height:150px;">
-            <p>Click 'Add Employee'</p>
-            <img src="help-imgs/add-emp 4.PNG" style="width:700px;height:200px;">
-            <p>When an employee is added, a confirmation message will
-            appear.</p>
+            <img src="help-imgs/add-emp22.PNG" style="width:900px;height:250px;">
+            <p>Enter the first, last name of the employee and select an employee 
+            skill. Note that both first and last name fields are required.
+            An employee may have any number of skills.</p>
+            <img src="help-imgs/add-emp33.PNG" style="width:900px;height:250px;">
+            <p>Click 'Add Employee'. When an employee is added, a confirmation message
+            will appear.</p>
         </div>
         
         <div>
             <h2>Adjusting Worker Availability</h2>
         </div>
         <div>
-            <h3>To view your employees' booking slots on the mainpage and the 
+            <p>To view your employees' booking slots on the mainpage and the 
             combined employee view, times must be set for an employee's
-            working hours.</h3>
+            working hours.</p>
         </div>
         <div>
             <img src="help-imgs/worker-available.PNG" style="width:700px;height:90px;">
@@ -62,6 +62,26 @@ class OwnerHelpPage    {
             <img src="help-imgs/worker-available3.PNG" style="width:900px;height:150px;">
             <p>Add the date and time the employee will be available and press
             submit.</p>
+        </div>
+        <div>
+            <h2>Create a Booking for a Customer</h2>
+        </div>
+        <div>
+            <p>Bookings can be made on behalf of the customer, provided that they
+            are registered in the system. The customer's email is needed to book on
+            their behalf.</p>
+        </div>
+        <div>
+            <img src="help-imgs/how3makebook.PNG" style="width:900px;height:210px;">
+            <p>Select an appointment type and an employee</p>
+            <img src="help-imgs/how3makebook1.PNG" style="width:900px;height:300px;">
+            <p>Select a time by clicking on an available slot</p>
+            <img src="help-imgs/bookascustomer.PNG" style="width:900px;height:240px;">
+            <p>Enter a valid customer email. Note that if a customer cannot be found
+            an error message will appear.</p>
+            <img src="help-imgs/bookascustomer2.PNG" style="width:900px;height:210px;">
+            <p>A confirmation message will appear if successful.</p>
+            
         </div>
         
         <div>
