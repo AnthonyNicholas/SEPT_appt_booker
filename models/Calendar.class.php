2e11f58e03b490bf2b458873998149ba962a25dd
Anthony Nicholas <s3607162@student.rmit.edu.au>
2017-05-21 13:19:30 +0000

    Fixed bug - appointments not disappearing when long appointment booked.

--- models/Calendar.class.php
+++ models/Calendar.class.php
@@ -46,6 +46,25 @@ class Calendar
       }
       
       // Query should select available times from given employee
+      // $query = "SELECT w.empID as calendar_id, t.dateTime as timestamp,
+      //         '' as firstname,
+      //         '' as lastname,
+      //         '' as email,
+      //         '' as phone,
+      //         0 as booked,
+      //         0 as noticed,
+      //         '' as deleted
+      //         FROM CanWork w, TimeSlot t,
+      //         WHERE w.dateTime = t.dateTime
+      //         AND w.dateTime NOT IN (
+      //             SELECT dateTime
+      //             FROM Booking
+      //         )
+      //         AND w.empID = ?
+      //         AND w.empID IN (SELECT empID FROM haveSkill)
+      //         ORDER BY t.dateTime ASC;";
+      
+      // Query should select available times from given employee
       $query = "SELECT w.empID as calendar_id, t.dateTime as timestamp,
               '' as firstname,
               '' as lastname,
@@ -56,10 +75,11 @@ class Calendar
               '' as deleted
               FROM CanWork w, TimeSlot t
               WHERE w.dateTime = t.dateTime
-              AND w.dateTime NOT IN (
-                  SELECT dateTime
-                  FROM Bookings
-              )
+              AND NOT EXISTS (
+                  SELECT *
+                  FROM Bookings b1
+                  WHERE w.dateTime >= b1.startTime
+                  AND w.dateTime < b1.endTime)
               AND w.empID = ?
               AND w.empID IN (SELECT empID FROM haveSkill)
               ORDER BY t.dateTime ASC;";
